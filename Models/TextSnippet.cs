e12c5669e4f0618e4139e56aee429e9ba93a176b
Anthony Hill <anthony.hill@eonenergy.com>
2025-06-11 13:47:35 +0100

    initial

---
+++ Models/TextSnippet.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.VectorData;
+using Microsoft.SemanticKernel.Data;
+
+namespace RAGTest.Models
+{
+    public sealed class TextSnippet
+    {
+        [VectorStoreKey]
+        public required string Key { get; set; }
+
+        [TextSearchResultValue]
+        [VectorStoreData]
+        public string? Text { get; set; }
+
+        [TextSearchResultName]
+        [VectorStoreData]
+        public string? ReferenceDescription { get; set; }
+
+        [TextSearchResultLink]
+        [VectorStoreData]
+        public string? ReferenceLink { get; set; }
+
+        [VectorStoreVector(1536)]
+        public string? TextEmbedding => this.Text;
+    }
+}
